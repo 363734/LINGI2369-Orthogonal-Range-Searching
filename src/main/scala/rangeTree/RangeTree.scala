d8d4df474eafc55662628678e1a4b5d0fff6a684
helene <helene.verhaeghe@student.uclouvain.be>
2015-04-29 21:04:15 +0200

    refactoring

--- src/main/scala/rangeTree/RangeTree.scala
+++ src/main/scala/rangeTree/RangeTree.scala
@@ -75,8 +75,9 @@ object RangeTree {
       val pivot = sortedPoints(0)(sublength)
       val pivotCode = (pivot.coord(depth), pivot.id)
 
-      val leftSortedPoints = sortedPoints.map(data => data.filter(d => ord.lt(d.coord(depth), pivotCode._1) || ((ord.equiv(d.coord(depth), pivotCode._1) && d.id <= pivotCode._2))))
-      val rightSortedPoints = sortedPoints.map(data => data.filter(d => !((ord.lt(d.coord(depth), pivotCode._1) || ((ord.equiv(d.coord(depth), pivotCode._1) && d.id <= pivotCode._2))))))
+      val leftSortedPoints = sortedPoints.map(data => data.filter(d => d.<=(pivot, depth)))
+      val rightSortedPoints = sortedPoints.map(data => data.filter(d => !(d.<=(pivot, depth))))
+
       RangeNode(pivot, associatedTree, depth, RangeTree.buildRangeTree(leftSortedPoints, depth, dim), RangeTree.buildRangeTree(rightSortedPoints, depth, dim))
     }
   }
